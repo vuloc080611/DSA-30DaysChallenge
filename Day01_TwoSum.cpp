2eaf3e39339531ade719584187d9dd7151576526
only4u <vuloc668165@gmail.com>
2026-05-29 11:37:24 +0700

    Create Day01_TwoSum.cpp

---
+++ Day01_TwoSum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> seen;
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        if (seen.count(complement)) return {seen[complement], i};
+        seen[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    auto res = twoSum(nums, target);
+    cout << "[" << res[0] << ", " << res[1] << "]" << endl; // [0,1]
+    return 0;
+}
