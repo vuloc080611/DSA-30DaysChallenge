3747f7819d6644013f7c20905d15e5c83742c187
only4u <vuloc668165@gmail.com>
2026-05-30 11:27:05 +0700

    Update Day05_BestTimeToBuyAndSellStock.cpp

--- Day05_BestTimeToBuyAndSellStock.cpp
+++ Day05_BestTimeToBuyAndSellStock.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX, profit = 0;
+    for (int p : prices) {
+        minPrice = min(minPrice, p);
+        profit = max(profit, p - minPrice);
+    }
+    return profit;
+}
+
+int main() {
+    vector<int> prices = {7,1,5,3,6,4};
+    cout << maxProfit(prices) << endl; // 5
+    return 0;
+}
