6af581461530e96b981df35b8f00123e62ac85f7
only4u <vuloc668165@gmail.com>
2026-05-30 11:26:08 +0700

    Update Day03_ReverseLinkedList.cpp

--- Day03_ReverseLinkedList.cpp
+++ Day03_ReverseLinkedList.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    while (head) {
+        ListNode* next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+
+void printList(ListNode* head) {
+    while (head) { cout << head->val << " "; head = head->next; }
+    cout << endl;
+}
+
+int main() {
+    // Tạo list 1->2->3->4->5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    printList(reverseList(head)); // 5 4 3 2 1
+    return 0;
+}
