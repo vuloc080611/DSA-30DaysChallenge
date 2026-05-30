1ade843adbeabeafd61fd3b64b43286a90e8b806
only4u <vuloc668165@gmail.com>
2026-05-30 11:26:47 +0700

    Update Day04_MergeTwoSortedLists.cpp

--- Day04_MergeTwoSortedLists.cpp
+++ Day04_MergeTwoSortedLists.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1->val < l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
+
+void printList(ListNode* head) {
+    while (head) { cout << head->val << " "; head = head->next; }
+    cout << endl;
+}
+
+int main() {
+    ListNode* l1 = new ListNode(1); l1->next = new ListNode(2); l1->next->next = new ListNode(4);
+    ListNode* l2 = new ListNode(1); l2->next = new ListNode(3); l2->next->next = new ListNode(4);
+    printList(mergeTwoLists(l1, l2)); // 1 1 2 3 4 4
+    return 0;
+}
