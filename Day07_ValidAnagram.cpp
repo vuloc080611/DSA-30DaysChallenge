bca07139dcd0e8a264309c981d65564355fd8765
only4u <vuloc668165@gmail.com>
2026-06-04 10:19:15 +0700

    Implement function to check if two strings are anagrams

--- Day07_ValidAnagram.cpp
+++ Day07_ValidAnagram.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) return false;
+    int cnt[26] = {0};
+    for (char c : s) cnt[c - 'a']++;
+    for (char c : t) if (--cnt[c - 'a'] < 0) return false;
+    return true;
+}
+
+int main() {
+    cout << (isAnagram("anagram", "nagaram") ? "true" : "false") << endl; // true
+    return 0;
+}
