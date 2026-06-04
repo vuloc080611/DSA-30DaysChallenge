10e26cffbbf19e177c16e1b7c0f3e796b29eaaba
only4u <vuloc668165@gmail.com>
2026-06-04 10:18:52 +0700

    Implement function to check for duplicate elements

--- Day06_ContainsDuplicate.cpp
+++ Day06_ContainsDuplicate.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
 
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> s;
+    for (int x : nums) {
+        if (s.count(x)) return true;
+        s.insert(x);
+    }
+    return false;
+}
+
+int main() {
+    vector<int> v = {1,2,3,1};
+    cout << (containsDuplicate(v) ? "true" : "false") << endl; // true
+    return 0;
+}
