e6e1cefe33e6b73eadec50efc6a9b01e8e9c0802
only4u <vuloc668165@gmail.com>
2026-06-04 10:19:54 +0700

    Add productExceptSelf function and main for testing
    Implement productExceptSelf function to calculate the product of array elements except self.

--- Day09_ProductOfArrayExceptSelf.cpp
+++ Day09_ProductOfArrayExceptSelf.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> res(n, 1);
+    int left = 1;
+    for (int i = 0; i < n; ++i) {
+        res[i] = left;
+        left *= nums[i];
+    }
+    int right = 1;
+    for (int i = n-1; i >= 0; --i) {
+        res[i] *= right;
+        right *= nums[i];
+    }
+    return res;
+}
+
+int main() {
+    vector<int> a = {1,2,3,4};
+    auto res = productExceptSelf(a);
+    for (int x : res) cout << x << " "; // 24 12 8 6
+    return 0;
+}
