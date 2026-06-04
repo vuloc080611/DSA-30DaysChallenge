4adeb3b2ebf3f8cad699776bd2f8804aa7c9ff79
only4u <vuloc668165@gmail.com>
2026-06-04 10:19:33 +0700

    Implement maximum subarray algorithm in C++

--- Day08_MaximumSubarray.cpp
+++ Day08_MaximumSubarray.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int maxSubArray(vector<int>& nums) {
+    int curr = nums[0], mx = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+        curr = max(nums[i], curr + nums[i]);
+        mx = max(mx, curr);
+    }
+    return mx;
+}
+
+int main() {
+    vector<int> a = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << maxSubArray(a) << endl; // 6
+    return 0;
+}
