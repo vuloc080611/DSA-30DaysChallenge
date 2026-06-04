4eae487b384568d35514b13ba22648750a2b5471
only4u <vuloc668165@gmail.com>
2026-06-04 10:20:14 +0700

    Implement threeSum function to find triplets

--- Day10_3Sum.cpp
+++ Day10_3Sum.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+vector<vector<int>> threeSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> res;
+    int n = nums.size();
+    for (int i = 0; i < n-2; ++i) {
+        if (i > 0 && nums[i] == nums[i-1]) continue;
+        int l = i+1, r = n-1;
+        while (l < r) {
+            int sum = nums[i] + nums[l] + nums[r];
+            if (sum == 0) {
+                res.push_back({nums[i], nums[l], nums[r]});
+                while (l < r && nums[l] == nums[l+1]) ++l;
+                while (l < r && nums[r] == nums[r-1]) --r;
+                ++l; --r;
+            } else if (sum < 0) ++l;
+            else --r;
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<int> a = {-1,0,1,2,-1,-4};
+    auto res = threeSum(a);
+    for (auto& v : res) {
+        for (int x : v) cout << x << " ";
+        cout << endl;
+    }
+    return 0;
+}
