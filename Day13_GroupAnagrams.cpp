cb7e057baf85c8b13d6a9278ca0466cc961bf0fb
only4u <vuloc668165@gmail.com>
2026-06-06 10:19:27 +0700

    Add groupAnagrams function in Day13_GroupAnagrams.cpp
    Implement a function to group anagrams from a list of strings.

--- Day13_GroupAnagrams.cpp
+++ Day13_GroupAnagrams.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
 
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> mp;
+    for (string& s : strs) {
+        string key = s;
+        sort(key.begin(), key.end());
+        mp[key].push_back(s);
+    }
+    vector<vector<string>> res;
+    for (auto& p : mp) res.push_back(p.second);
+    return res;
+}
+
+int main() {
+    vector<string> words = {"eat","tea","tan","ate","nat","bat"};
+    auto res = groupAnagrams(words);
+    for (auto& group : res) {
+        for (string& w : group) cout << w << " ";
+        cout << endl;
+    }
+    return 0;
+}
