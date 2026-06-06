ac0cd27c9ad03227d0d266d2ca216d5f93e498bc
only4u <vuloc668165@gmail.com>
2026-06-06 10:19:09 +0700

    Add function for longest substring without repeating chars
    Implement function to find length of longest substring without repeating characters.

--- Day12_LongestSubstringWithoutRepeatingCharacters.cpp
+++ Day12_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
 
+int lengthOfLongestSubstring(string s) {
+    unordered_set<char> win;
+    int l = 0, mx = 0;
+    for (int r = 0; r < s.size(); ++r) {
+        while (win.count(s[r])) win.erase(s[l++]);
+        win.insert(s[r]);
+        mx = max(mx, r - l + 1);
+    }
+    return mx;
+}
+
+int main() {
+    cout << lengthOfLongestSubstring("abcabcbb") << endl; // 3
+    return 0;
+}
