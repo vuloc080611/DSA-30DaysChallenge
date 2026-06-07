2551f42706a5891d4c9a1acbc1b24b2486d3055d
only4u <vuloc668165@gmail.com>
2026-06-07 12:27:57 +0700

    Add spiralOrder function to traverse matrix
    Implement spiral order traversal for a matrix.

--- Day18_SpiralMatrix.cpp
+++ Day18_SpiralMatrix.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> res;
+    int top = 0, bottom = matrix.size()-1;
+    int left = 0, right = matrix[0].size()-1;
+    while (top <= bottom && left <= right) {
+        for (int j = left; j <= right; ++j) res.push_back(matrix[top][j]);
+        top++;
+        for (int i = top; i <= bottom; ++i) res.push_back(matrix[i][right]);
+        right--;
+        if (top <= bottom) {
+            for (int j = right; j >= left; --j) res.push_back(matrix[bottom][j]);
+            bottom--;
+        }
+        if (left <= right) {
+            for (int i = bottom; i >= top; --i) res.push_back(matrix[i][left]);
+            left++;
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<vector<int>> m = {{1,2,3},{4,5,6},{7,8,9}};
+    auto res = spiralOrder(m);
+    for (int x : res) cout << x << " "; // 1 2 3 6 9 8 7 4 5
+    return 0;
+}
