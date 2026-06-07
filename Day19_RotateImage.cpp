069e55ed8729b7205e19edaf8168a252ee6e3ca9
only4u <vuloc668165@gmail.com>
2026-06-07 12:28:13 +0700

    Implement matrix rotation function

--- Day19_RotateImage.cpp
+++ Day19_RotateImage.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; ++i)
+        for (int j = i; j < n; ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    for (int i = 0; i < n; ++i)
+        reverse(matrix[i].begin(), matrix[i].end());
+}
+
+int main() {
+    vector<vector<int>> m = {{1,2,3},{4,5,6},{7,8,9}};
+    rotate(m);
+    for (auto& row : m) {
+        for (int x : row) cout << x << " ";
+        cout << endl;
+    }
+    return 0;
+}
