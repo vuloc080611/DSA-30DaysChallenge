1f999025037c594787ae9da3ea191cbc1f3ff430
only4u <vuloc668165@gmail.com>
2026-06-07 12:27:25 +0700

    Add minWindow function to find minimum substring
    Implement minimum window substring algorithm.

--- Day16_MinimumWindowSubstring.cpp
+++ Day16_MinimumWindowSubstring.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+using namespace std;
 
+string minWindow(string s, string t) {
+    vector<int> need(128,0);
+    for (char c : t) need[c]++;
+    int missing = t.size(), l = 0, start = 0, minLen = INT_MAX;
+    for (int r = 0; r < s.size(); ++r) {
+        if (need[s[r]]-- > 0) missing--;
+        while (missing == 0) {
+            if (r - l + 1 < minLen) {
+                minLen = r - l + 1;
+                start = l;
+            }
+            if (++need[s[l]] > 0) missing++;
+            l++;
+        }
+    }
+    return minLen == INT_MAX ? "" : s.substr(start, minLen);
+}
+
+int main() {
+    cout << minWindow("ADOBECODEBANC", "ABC") << endl; // "BANC"
+    return 0;
+}
