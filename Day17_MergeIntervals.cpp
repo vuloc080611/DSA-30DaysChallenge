9da4a1075327fa710233eb20b82e87326b88ca28
only4u <vuloc668165@gmail.com>
2026-06-07 12:27:40 +0700

    Implement merge intervals function in C++

--- Day17_MergeIntervals.cpp
+++ Day17_MergeIntervals.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> res;
+    for (auto& inv : intervals) {
+        if (res.empty() || res.back()[1] < inv[0])
+            res.push_back(inv);
+        else
+            res.back()[1] = max(res.back()[1], inv[1]);
+    }
+    return res;
+}
+
+int main() {
+    vector<vector<int>> intervals = {{1,3},{2,6},{8,10},{15,18}};
+    auto res = merge(intervals);
+    for (auto& v : res) cout << "[" << v[0] << "," << v[1] << "] ";
+    // [1,6] [8,10] [15,18]
+    return 0;
+}
