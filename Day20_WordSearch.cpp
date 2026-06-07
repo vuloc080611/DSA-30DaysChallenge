188b4ae8eeb212f75fccc19c202dda1f0dfa1b58
only4u <vuloc668165@gmail.com>
2026-06-07 12:28:35 +0700

    Add word search functionality using DFS
    Implement depth-first search to check word existence in a board.

--- Day20_WordSearch.cpp
+++ Day20_WordSearch.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx) {
+    if (idx == word.size()) return true;
+    if (i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[idx]) return false;
+    char c = board[i][j];
+    board[i][j] = '#';
+    bool found = dfs(board, word, i+1, j, idx+1) || dfs(board, word, i-1, j, idx+1)
+              || dfs(board, word, i, j+1, idx+1) || dfs(board, word, i, j-1, idx+1);
+    board[i][j] = c;
+    return found;
+}
+
+bool exist(vector<vector<char>>& board, string word) {
+    for (int i = 0; i < board.size(); ++i)
+        for (int j = 0; j < board[0].size(); ++j)
+            if (dfs(board, word, i, j, 0)) return true;
+    return false;
+}
+
+int main() {
+    vector<vector<char>> board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    cout << (exist(board, "ABCCED") ? "true" : "false") << endl; // true
+    return 0;
+}
