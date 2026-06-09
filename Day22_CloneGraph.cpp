d58f1ca040e11fd1b0bf9f05a74f3d4715b0f298
only4u <vuloc668165@gmail.com>
2026-06-09 09:48:12 +0700

    Add graph cloning function with test case
    Implement a function to clone a graph represented by nodes and their neighbors. Includes a simple test case in main.

--- Day22_CloneGraph.cpp
+++ Day22_CloneGraph.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() : val(0) {}
+    Node(int _val) : val(_val) {}
+    Node(int _val, vector<Node*> _neighbors) : val(_val), neighbors(_neighbors) {}
+};
+
+unordered_map<Node*, Node*> cloned;
+Node* cloneGraph(Node* node) {
+    if (!node) return nullptr;
+    if (cloned.count(node)) return cloned[node];
+    Node* copy = new Node(node->val);
+    cloned[node] = copy;
+    for (Node* nbr : node->neighbors)
+        copy->neighbors.push_back(cloneGraph(nbr));
+    return copy;
+}
+
+// Test đơn giản
+int main() {
+    Node* n1 = new Node(1);
+    Node* n2 = new Node(2);
+    n1->neighbors.push_back(n2);
+    n2->neighbors.push_back(n1);
+    Node* copy = cloneGraph(n1);
+    cout << copy->val << " " << copy->neighbors[0]->val << endl; // 1 2
+    return 0;
+}
