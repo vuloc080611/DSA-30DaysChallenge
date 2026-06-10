57c53b64150cee1f0f9285e120ae4e9f3ec46c60
only4u <vuloc668165@gmail.com>
2026-06-10 12:13:03 +0700

    Implement maxCoins function for balloon bursting

--- Day30_BurstBalloons.cpp
+++ Day30_BurstBalloons.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int maxCoins(vector<int>& nums) {
+    int n = nums.size();
+    nums.insert(nums.begin(), 1);
+    nums.push_back(1);
+    vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+    for (int len = 1; len <= n; ++len) {
+        for (int i = 1; i <= n - len + 1; ++i) {
+            int j = i + len - 1;
+            for (int k = i; k <= j; ++k) {
+                dp[i][j] = max(dp[i][j], dp[i][k-1] + dp[k+1][j] +
+                               nums[i-1] * nums[k] * nums[j+1]);
+            }
+        }
+    }
+    return dp[1][n];
+}
+
+int main() {
+    vector<int> balloons = {3,1,5,8};
+    cout << maxCoins(balloons) << endl; // 167
+    return 0;
+}
