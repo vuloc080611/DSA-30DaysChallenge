1ed4c9a3db50c6a16d85b49dd37e1874435af8e2
only4u <vuloc668165@gmail.com>
2026-06-10 12:12:49 +0700

    Implement coin change algorithm in C++

--- Day29_CoinChange.cpp
+++ Day29_CoinChange.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
 
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount+1, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i)
+        for (int c : coins)
+            if (i - c >= 0 && dp[i-c] != INT_MAX)
+                dp[i] = min(dp[i], dp[i-c] + 1);
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
+}
+
+int main() {
+    vector<int> coins = {1,2,5};
+    cout << coinChange(coins, 11) << endl; // 3 (5+5+1)
+    return 0;
+}
