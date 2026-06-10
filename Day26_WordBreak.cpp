38927fa354717019e4f33de8dba0a753f04e43a4
only4u <vuloc668165@gmail.com>
2026-06-10 12:11:54 +0700

    Implement word break algorithm in C++

--- Day26_WordBreak.cpp
+++ Day26_WordBreak.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
 
+bool wordBreak(string s, vector<string>& wordDict) {
+    unordered_set<string> dict(wordDict.begin(), wordDict.end());
+    vector<bool> dp(s.size()+1, false);
+    dp[0] = true;
+    for (int i = 1; i <= s.size(); ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (dp[j] && dict.count(s.substr(j, i-j))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp.back();
+}
+
+int main() {
+    string s = "leetcode";
+    vector<string> dict = {"leet","code"};
+    cout << (wordBreak(s, dict) ? "true" : "false") << endl; // true
+    return 0;
+}
