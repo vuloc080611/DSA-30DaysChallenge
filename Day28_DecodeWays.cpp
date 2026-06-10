b0494b5c772f3601309615de0f6e849aa3938d67
only4u <vuloc668165@gmail.com>
2026-06-10 12:12:33 +0700

    Add numDecodings function for decoding ways
    Implement function to decode ways from a string of digits.

--- Day28_DecodeWays.cpp
+++ Day28_DecodeWays.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+int numDecodings(string s) {
+    if (s[0] == '0') return 0;
+    int n = s.size();
+    vector<int> dp(n+1, 0);
+    dp[0] = 1; dp[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        int one = stoi(s.substr(i-1, 1));
+        if (one >= 1 && one <= 9) dp[i] += dp[i-1];
+        int two = stoi(s.substr(i-2, 2));
+        if (two >= 10 && two <= 26) dp[i] += dp[i-2];
+    }
+    return dp[n];
+}
+
+int main() {
+    cout << numDecodings("226") << endl; // 3 (BBF, BZ, VF)
+    return 0;
+}
