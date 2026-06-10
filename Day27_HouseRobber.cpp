9d26eb860bf4d6ab69369e3a152a8ede8801d3ba
only4u <vuloc668165@gmail.com>
2026-06-10 12:12:11 +0700

    Add house robber solution in Day27_HouseRobber.cpp
    Implement house robber algorithm to maximize loot.

--- Day27_HouseRobber.cpp
+++ Day27_HouseRobber.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int rob(vector<int>& nums) {
+    if (nums.empty()) return 0;
+    int prev2 = 0, prev1 = 0;
+    for (int x : nums) {
+        int cur = max(prev1, prev2 + x);
+        prev2 = prev1;
+        prev1 = cur;
+    }
+    return prev1;
+}
+
+int main() {
+    vector<int> a = {2,7,9,3,1};
+    cout << rob(a) << endl; // 12
+    return 0;
+}
